9f0182c91bc58cbb5e2ecb104ffd00ed8a5b54fa
Mrbysco <Mrbysco@users.noreply.github.com>
2026-03-29 16:56:48 +0200

    Port to 1.21.11

--- src/main/java/com/mrbysco/mimicworld/MimicWorldMod.java
+++ src/main/java/com/mrbysco/mimicworld/MimicWorldMod.java
@@ -4,8 +4,8 @@ import com.mojang.logging.LogUtils;
 import com.mrbysco.mimicworld.handler.PortalHandler;
 import com.mrbysco.mimicworld.registry.MimicRegistry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -17,7 +17,7 @@ public class MimicWorldMod {
 	public static final String MOD_ID = "mimicworld";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public static final ResourceKey<Level> MIMIC_WORLD_KEY = ResourceKey.create(Registries.DIMENSION, ResourceLocation.fromNamespaceAndPath(MOD_ID, "mimic_world"));
+	public static final ResourceKey<Level> MIMIC_WORLD_KEY = ResourceKey.create(Registries.DIMENSION, Identifier.fromNamespaceAndPath(MOD_ID, "mimic_world"));
 
 	public MimicWorldMod(IEventBus eventBus) {
 		MimicRegistry.BLOCKS.register(eventBus);
